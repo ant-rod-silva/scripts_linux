a9ddf0c197bdb5e5f24133a737a498305c543211
Antonio Rodrigo dos Santos Silva <20875150+rodrigosantosbr@users.noreply.github.com>
2018-06-19 16:06:18 -0300

    Add files via upload

---
+++ backup_db_cron.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+COR=`tput setaf 3`
+NC=`tput sgr0` # No Color
+sh -c "clear"
+
+# crontab -e:
+# At 09:05
+# 0 9 * * * echo 'sh /home/<user>/<script_location_dir>/backup_db_cron.sh' | /bin/bash
+
+
+# Location to place backups.
+backup_dir="/home/<user>/<target_dir>"
+
+#Numbers of days you want to keep a copy of your database
+number_of_days=30
+
+echo "${COR}DB Backup${NC}"
+/usr/bin/pg_dump --host=000.000.111.111 --username=user --schema=public -O -v > $backup_dir/backup_file_name_$(date +%d_%m_%Y_%H_%M).sql
+
+echo "${COR}Removing old files (> 30 days)${NC}"
+find $backup_dir -type f -prune -mtime +$number_of_days | xargs rm -rf
